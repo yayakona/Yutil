dabe31b15fa2accfe1abb596eb046c2b95ce5c87
yayakona <44542602+yayakona@users.noreply.github.com>
2019-04-28 14:35:04 +0900

    Update IOUtil.py get_list_for_file

--- Yutil/basic/IOUtil.py
+++ Yutil/basic/IOUtil.py
@@ -7,3 +7,6 @@ def input_line(type_= str):
 def print_2d_list(lst2):
     "¥n".join(["".join(str(v) for v in lst) for lst in lst2])
 
+def get_list_from_file(path, encoding=“UTF-8”):
+    with open(path, “r”, encoding=encoding) as f:
+        return readlines.split()
